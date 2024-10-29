7f46e881ab502dbc9d13f07226744cae66f3421c
Joaquín Carrascal Franco <carrascal.frjoa24@triana.salesianos.edu>
2024-10-29 13:27:16 +0100

    en casa seguimos

--- PokemonBattle/src/app/app.module.ts
+++ PokemonBattle/src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideHttpClient } from '@angular/common/http';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { PokemonComponent } from './components/pokemon/pokemon.component';
 import { PokemonBattleComponent } from './components/pokemon-battle/pokemon-battle.component';
+import { LottieComponent, provideLottieOptions } from 'ngx-lottie';
 
 @NgModule({
   declarations: [
@@ -17,9 +17,13 @@ import { PokemonBattleComponent } from './components/pokemon-battle/pokemon-batt
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    LottieComponent
   ],
-  providers: [provideHttpClient()],
+  providers: [provideHttpClient(),
+    provideLottieOptions({
+    player: () => import('lottie-web'),
+    })],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- PokemonBattle/src/app/components/pokemon/pokemon.component.ts
+++ PokemonBattle/src/app/components/pokemon/pokemon.component.ts
@@ -1,6 +1,8 @@
-import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { PokemonResponse } from '../../model/ipokemon.interface';
 import { PokemonService } from '../../services/pokemon.service';
+import { AnimationOptions } from 'ngx-lottie';
+import { AnimationItem } from 'ngx-lottie/lib/symbols';
 
 @Component({
   selector: 'app-pokemon',
@@ -14,6 +16,14 @@ export class PokemonComponent{
 
   id: number | undefined;
   pokemonHealth: number = 100;
+
+  options: AnimationOptions = {
+    path: 'Animation - 1730203042898.json',
+  };
+
+  animationCreated(animationItem: AnimationItem): void {
+    console.log(animationItem);
+  }
 
   constructor(private pokemonService: PokemonService) { }
 
