ac10c554274b706f030d695f2ca2f0475473554e
Joaquín Carrascal Franco <carrascal.frjoa24@triana.salesianos.edu>
2024-12-03 19:29:58 +0100

    Enhance main application layout with color selection and rating slider

--- Flutter/helloapp/lib/main.dart
+++ Flutter/helloapp/lib/main.dart
@@ -7,7 +7,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -32,6 +31,10 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  var rating = 0.0;
+  Color _selectedColor = Colors.red;
+  var textoBoton = 'Botón pulsado';
+  int counterBtn = 0;
 
   void _incrementCounter() {
     setState(() {
@@ -46,25 +49,81 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      body: Container(
+        color: _selectedColor,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                'You have pushed the button this many times:',
+              ),
+              Text(
+                '$_counter',
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+              Text(
+                '$textoBoton $counterBtn veces',
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+              DropdownMenu<Color>(
+                menuStyle: const MenuStyle(
+                  backgroundColor: WidgetStatePropertyAll<Color>(Colors.white),
+                ),
+                dropdownMenuEntries: const [
+                  DropdownMenuEntry(
+                    value: Colors.red,
+                    label: 'Red',
+                  ),
+                  DropdownMenuEntry(
+                    value: Colors.green,
+                    label: 'Green',
+                  ),
+                  DropdownMenuEntry(
+                    value: Colors.blue,
+                    label: 'Blue',
+                  ),
+                ],
+                onSelected: (Color? newColor) {
+                  if (newColor != null) {
+                    setState(() {
+                      _selectedColor = newColor;
+                    });
+                  }
+                },
+              ),
+              Slider(
+                value: rating,
+                min: 0,
+                max: 100.0,
+                onChanged: (double newRating) {
+                  setState(() {
+                    rating = newRating;
+                  });
+                },
+                divisions: 5,
+                label: rating.toString(),
+              ),
+              IconButton(
+                icon: const Icon(Icons.info),
+                iconSize: 40,
+                tooltip: 'Soy un tooltip',
+                onPressed: () {
+                  setState(() {
+                    textoBoton = 'Botón pulsado';
+                    counterBtn++;
+                  });
+                },
+              )
+            ],
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
